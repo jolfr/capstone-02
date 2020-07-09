cd40e3ee47fffa355bc2d391e8aabb5ec356fd9d
Jolfr <thomas.jack.carroll@gmail.com>
2020-07-09 10:14:18 -0400

    updated nation download file type to .dta

--- src/data/download_nation_data.py
+++ src/data/download_nation_data.py
@@ -6,7 +6,7 @@ print('Beginning file download with requests')
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 url = 'https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/BMRPVH/SPI1XO'
-filename = '../../data/external/nation_data.sta'
+filename = '../../data/external/nation_data.dta'
 r = requests.get(url)
 
 with open(filename, 'wb') as f:
