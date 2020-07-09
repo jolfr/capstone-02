9d57d1873a1a225efe6e36d5b367c704df8dac39
Jolfr <thomas.jack.carroll@gmail.com>
2020-07-09 08:59:35 -0400

    cookiecutter data science project files generated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Capstone for Springboard DS track: Prediction of future growth of entrepreneurial environments by zip code.',
+    author='jolfr',
+    license='MIT',
+)
