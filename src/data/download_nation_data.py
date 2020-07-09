411a4b8cd71fada1d6572ddf25f56fdec668722d
Jolfr <thomas.jack.carroll@gmail.com>
2020-07-09 11:01:02 -0400

    fixed errors in download national data script

--- src/data/download_nation_data.py
+++ src/data/download_nation_data.py
@@ -1,15 +1,14 @@
-from src.data.download_from_dataverse import download
-
-
-def main():
-    status, headers, encoding = download_nation()
-    print('STATUS:\n' + status)
-    print('HEADERS:\n' + headers)
-    print('ENCODING:\n' + encoding)
+from src.data import download_from_dataverse as dataverse
 
 
 def download_nation():
     doi = '10.7910/DVN/BMRPVH/SPI1XO'
     filename = 'nation_data.dta'
-    status, headers, encoding = download(doi, filename)
+    status, headers, encoding = dataverse.download(doi=doi, filename=filename)
     return status, headers, encoding
+
+
+s, h, e = download_nation()
+print('STATUS:\t\t' + str(s))
+print('HEADERS:\t' + h)
+print('ENCODING:\t' + e)
