90acc423264b7ee131ffbd9f1dda841188c80b23
Jolfr <thomas.jack.carroll@gmail.com>
2020-07-09 11:12:31 -0400

    zip data download script completed

---
+++ src/data/download_zip_data.py
@@ -0,0 +1,14 @@
+from src.data import download_from_dataverse as dataverse
+
+
+def download_zip():
+    doi = '10.7910/DVN/BMRPVH/N2FWBK'
+    filename = 'zip_data.dta'
+    status, headers, encoding = dataverse.download(doi=doi, filename=filename)
+    return status, headers, encoding
+
+
+s, h, e = download_zip()
+print('STATUS:\t\t' + str(s))
+print('HEADERS:\t' + h)
+print('ENCODING:\t' + e)
