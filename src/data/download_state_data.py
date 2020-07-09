f78716f1cd21253490a165ccfb2ae0ba0150e068
Jolfr <thomas.jack.carroll@gmail.com>
2020-07-09 11:04:00 -0400

    state data download script completed

---
+++ src/data/download_state_data.py
@@ -0,0 +1,14 @@
+from src.data import download_from_dataverse as dataverse
+
+
+def download_state():
+    doi = '10.7910/DVN/BMRPVH/MR5DYF'
+    filename = 'state_data.dta'
+    status, headers, encoding = dataverse.download(doi=doi, filename=filename)
+    return status, headers, encoding
+
+
+s, h, e = download_state()
+print('STATUS:\t\t' + str(s))
+print('HEADERS:\t' + h)
+print('ENCODING:\t' + e)
