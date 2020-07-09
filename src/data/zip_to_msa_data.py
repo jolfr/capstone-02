bf2df85dadc1d5c96c741288ba9c79acc01798fc
Jolfr <thomas.jack.carroll@gmail.com>
2020-07-09 17:22:58 -0400

    zip to msa data download script added

---
+++ src/data/zip_to_msa_data.py
@@ -0,0 +1,24 @@
+import os
+import requests
+
+
+def download():
+    print('Beginning ZCTA to MSA download with requests')
+
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    url = 'https://www2.census.gov/geo/docs/maps-data/data/rel/zcta_cbsa_rel_10.txt'
+    filename = '../../data/external/zip_to_msa.csv'
+    r = requests.get(url)
+    r.raise_for_status()
+
+    with open(filename, 'wb') as f:
+        f.write(r.content)
+
+    return r.status_code, r.headers['content-type'], r.encoding
+
+
+status, headers, encoding = download()
+print(status)
+print(headers)
+print(encoding)
