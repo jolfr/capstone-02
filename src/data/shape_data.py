24912f74386daf4c5d27dfe555b37f5e94ce27ba
Jolfr <thomas.jack.carroll@gmail.com>
2020-07-30 17:08:14 -0400

    updated shape zip data

--- src/data/shape_data.py
+++ src/data/shape_data.py
@@ -2,15 +2,13 @@ import pandas as pd
 import numpy as nd
 
 
-def fill_years(df):
+def generate_filled(df):
     years = pd.DataFrame(range(1988, 2017))
     years.columns = ['year']
     unique = df.zipcode.unique()
     result = pd.DataFrame()
     for zipcode in unique:
         append = years.copy()
-        state = df.loc[df['zipcode'] == zipcode, 'state'].iloc[0]
         append['zipcode'] = zipcode
-        append['state'] = state
         result = pd.concat([result, append], ignore_index=True)
     return result
